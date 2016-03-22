199992bbcce46121d11b522388c432bacce54bb8
Bohdan Rybak <bohdan.rybak@gmail.com>
2016-03-22 22:54:44 +0200

    Round Key derivation function

--- src/common.h
+++ src/common.h
@@ -6,11 +6,11 @@
 #define DIGSIG_COMMON_H
 
 #include <array>
-const size_t block_size = 64;
-const size_t misty_block_size = 32;
+const size_t block_size = 8; // bytes
+const size_t misty_block_size = block_size / 2;
 
 typedef unsigned char byte_t;
-typedef std::array<byte_t, block_size>         block_t; // 8 bytes
-typedef std::array<byte_t, misty_block_size>   mblock_t; // 4 bytes
+typedef std::array<byte_t, block_size>         block_t;
+typedef std::array<byte_t, misty_block_size>   mblock_t;
 
 #endif //DIGSIG_COMMON_H
--- src/misty.cpp
+++ src/misty.cpp
@@ -5,7 +5,33 @@
 
 mblock_t get_round_key(const block_t& i_key, size_t i_round)
   {
-  throw std::logic_error("Wrong round number");
+  mblock_t left, right;
+  mblock_t inverted_left, inverted_right;
+
+  std::copy(i_key.rbegin() + misty_block_size, i_key.rend(), left.begin());
+  std::copy(i_key.rbegin(), i_key.rbegin() + misty_block_size, right.begin());
+
+   switch(i_round)
+    {
+    case 0:
+      return left;
+
+    case 1:
+      return right;
+
+    case 2:
+      for(size_t i = 0; i < right.size(); i++)
+        inverted_right[i] = ~right[i];
+      return inverted_right;
+
+    case 3:
+      for(size_t i = 0; i < left.size(); i++)
+        inverted_left[i] = ~left[i];
+      return inverted_left;
+
+    default:
+      throw std::logic_error("Wrong round number");
+    }
   }
 
 block_t encrypt(const block_t &i_message, const block_t &i_key)
