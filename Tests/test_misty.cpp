4858539de575c7b37a2afc015f165b00f596432f
Bohdan Rybak <bohdan.rybak@gmail.com>
2016-03-23 11:17:19 +0200

    Fixed tests to use int values

--- Tests/test_misty.cpp
+++ Tests/test_misty.cpp
@@ -8,7 +8,7 @@
 
 TEST_CASE("Round key is generated", "[misty]")
   {
-  block_t key ({0x36, 0x73, 0x59, 0x4F, 0x47, 0x39, 0x61, 0x4E});
+  block_t key = 0x4E6139474F597336;
 
   REQUIRE_NOTHROW(get_round_key(key, 0));
 
@@ -26,19 +26,19 @@ TEST_CASE("Round key is generated", "[misty]")
   SECTION("Round key is correctly generated for all rounds")
     {
     auto key_0 = get_round_key(key, 0);
-    mblock_t ground_truth_key_0 = {0x4F, 0x59, 0x73, 0x36};
+    mblock_t ground_truth_key_0 = 0x4F597336;
     REQUIRE(key_0 == ground_truth_key_0);
 
     auto key_1 = get_round_key(key, 1);
-    mblock_t ground_truth_key_1 = {0x4E, 0x61, 0x39, 0x47};
+    mblock_t ground_truth_key_1 = 0x4E613947;
     REQUIRE(key_1 == ground_truth_key_1);
 
     auto key_2 = get_round_key(key, 2);
-    mblock_t ground_truth_key_2 = {0xB1, 0x9E, 0xC6, 0xB8};
+    mblock_t ground_truth_key_2 = 0xB19EC6B8;
     REQUIRE(key_2 == ground_truth_key_2);
 
     auto key_3 = get_round_key(key, 3);
-    mblock_t ground_truth_key_3 = {0xB0, 0xA6, 0x8C, 0xC9};
+    mblock_t ground_truth_key_3 = 0xB0A68CC9;
     REQUIRE(key_3 == ground_truth_key_3);
     }
 
